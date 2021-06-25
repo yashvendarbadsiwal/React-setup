bbd64a20d803bcede5907eeda5ebee4ed9df48cf
yashvendarbadsiwal <yashalwar4@gmail.com>
2021-06-25 11:31:32 +0530

    reactstrap Added

--- src/Components/temp1.jsx
+++ src/Components/temp1.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
 
 const Temp1 = () => {
     return (
         <div style={{backgroundColor:"green", width:"400px", height:"400px"}}>
             Hello yash!
+            <Container>
+                <Row>
+                    <Col md={{size:6, offset:2}}>
+                        HII
+                    </Col>
+                    <Col>
+                        , How are You?
+                    </Col>
+                </Row>
+                <Row>
+
+                </Row>
+            </Container>
         </div>
     );
 }
