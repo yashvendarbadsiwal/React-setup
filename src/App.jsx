3cecf130a2e2cf4b7acaae08c13bc6be5765125b
yashvendarbadsiwal <yashalwar4@gmail.com>
2021-06-25 11:23:48 +0530

    bootstrap added

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
-import { BrowserRouter, NavLink } from 'react-router-dom';
-import logo from './logo.svg';
-import './App.scss';
+import { BrowserRouter, NavLink } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.scss";
 
-import MainComponent from './Components/MainComponent';
-
+import MainComponent from "./Components/MainComponent";
 
 function App() {
   return (
     <div className="App">
-      
       <BrowserRouter>
-      <div className="navigationHeader">
-        <NavLink to="/home">Home</NavLink>
-        <NavLink to="/temp1">Temp1</NavLink>
-        <NavLink to="/temp2">Temp2</NavLink>
-        <NavLink to="/text">Text</NavLink>
-      </div>
+        <div className="navigationHeader">
+          <NavLink to="/home">Home</NavLink>
+          <NavLink to="/temp1">Temp1</NavLink>
+          <NavLink to="/temp2">Temp2</NavLink>
+          <NavLink to="/text">Text</NavLink>
+        </div>
         <MainComponent />
       </BrowserRouter>
-      
-      
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -34,6 +31,12 @@ function App() {
           Learn React
         </a>
       </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-4">Hii Yash</div>
+          <div className="col-6">Badsiwal</div>
+        </div>
+      </div>
     </div>
   );
 }
